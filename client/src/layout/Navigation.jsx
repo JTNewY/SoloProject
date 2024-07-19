bc6973804ae74580ff2897063979663e8f7faadf
JTNewY <niner2@naver.com>
2024-07-19 18:04:57 +0900

    일단 한거 저장

--- client/src/layout/Navigation.jsx
+++ client/src/layout/Navigation.jsx
@@ -14,7 +14,7 @@ const Navigation = () => (
           <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" to="/">Home</Link></li>
           <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" to="/about">About</Link></li>
           <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" to="/post">Sample Post</Link></li>
-          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" to="/contact">Contact</Link></li>
+          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" to="/login">Login</Link></li>
         </ul>
       </div>
     </div>
